574797dbffa382a6760b15f7bc24df81e6431364
20234111 <Sitthiporn_P@hinothailand.com>
2024-01-30 09:57:25 +0700

    +KBNCR220 Special Report

---
+++ wwwroot/assets/.script/ReceiveProcess/KBNCR220.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function () {
+    var pdsSet = new Set();
+
+    const table = $('#tblMaster').DataTable({
+        columns: [
+            { data: "No" },
+            { data: "F_OrderNo" },
+            { data: "F_Supplier" },
+            { data: "F_Delivery_Date" },
+            { data: "F_Receive_Date" },
+            { data: "F_PDS_Status" },
+            { data: "F_Receive_Status" }
+        ],
+        order: [[0, 'asc']],
+        paging: false,
+    });
+    xSplash.hide();
+});
