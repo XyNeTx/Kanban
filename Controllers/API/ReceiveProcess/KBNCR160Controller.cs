eafe9b57c24ab21cc7afe1670d34612b8ffba0f0
20234111 <Sitthiporn_P@hinothailand.com>
2024-02-02 17:28:42 +0700

    KBNCR160 : Complete

---
+++ Controllers/API/ReceiveProcess/KBNCR160Controller.cs
@@ -0,0 +1,70 @@
+﻿using HINOSystem.Context;
+using HINOSystem.Libs;
+using KANBAN.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
+namespace KANBAN.Controllers.API.ReceiveProcess
+{
+    public class KBNCR160Controller : Controller
+    {
+        private readonly IConfiguration _configuration;
+        private readonly BearerClass _BearerClass;
+        private readonly ActionResultClass _ActionResult;
+        private readonly KanbanConnection _KBCN;
+        private readonly PPMConnect _PPMConnect;
+        private readonly PPM3Context _PPM3Context;
+        private readonly PPMContext _PPMContext;
+        private readonly KB3Context _KB3Context;
+
+
+        private readonly string StoragePath = @"wwwroot\Storage\Uploads";
+
+        public KBNCR160Controller(
+            IConfiguration configuration,
+            BearerClass bearerClass,
+            ActionResultClass actionResultClass,
+            KanbanConnection kanbanConnection,
+            PPMConnect ppmConnect,
+            PPMContext pPMContext,
+            PPM3Context pPM3Context,
+            KB3Context kB3Context
+            )
+        {
+            _configuration = configuration;
+            _BearerClass = bearerClass;
+            _ActionResult = actionResultClass;
+            _KB3Context = kB3Context;
+            _KBCN = kanbanConnection;
+            _PPMConnect = ppmConnect;
+            _PPM3Context = pPM3Context;
+            _PPMContext = pPMContext;
+        }
+
+        public async Task<IActionResult> Initial()
+        {
+            try
+            {
+                string _result = "";
+                var supList = await _KB3Context.TB_MS_PartOrder.Select(x => new
+                {
+                    F_Part_No = x.F_Part_No + '-' + x.F_Ruibetsu
+                }).OrderBy(x => x.F_Part_No).Distinct().ToListAsync();
+
+                string _jsonData = JsonConvert.SerializeObject(supList);
+                _result = @"{
+                                ""status"":""200"",
+                                ""response"":""OK"",
+                                ""message"": ""Data Found"",
+                                ""data"": " + _jsonData + @"}";
+
+                return Ok(_result);
+            }
+            catch (Exception ex)
+            {
+                return Content(ex.ToString());
+            }
+        }
+    }
+}
---
+++ wwwroot/assets/.script/ReceiveProcess/KBNCR160.js
@@ -0,0 +1,51 @@
+﻿$(document).ready(function () {
+    xAjax.Post({
+        url: 'KBNCR160/Initial',
+        then: function (result) {
+            // console.log(result);
+            $.each(result.data, function (e, t) {
+                // console.log(e + " eeeeeeeeeeeee ", t.F_Supplier_Code + " tttttttttttt ")
+                $("#F_PartFrom").append($("<option>", { value: t.F_Part_No, text: t.F_Part_No }, "</option>"));
+                $("#F_PartTo").append($("<option>", { value: t.F_Part_No, text: t.F_Part_No }, "</option>"));
+            });
+            $('#supFromBlank').hide();
+            $('#supToBlank').hide();
+            xSplash.hide();
+        },
+        error: function (result) {
+            console.error("Initial Error from Receive Special Report");
+            xSplash.hide();
+        },
+    });
+
+    xAjax.onClick("#ReportBtn", function () {
+        var devDate = $("#F_DeliveryFrom").val().replaceAll('-', '');
+        var toDate = $("#F_DeliveryTo").val().replaceAll('-', '');
+
+        if (devDate > toDate) {
+            return alert("Please Don't select Delivery date to less than Delivery date from");
+        }
+
+        var partFrom = $("#F_PartFrom").val();
+        var partTo = $("#F_PartTo").val();
+
+        if (partFrom > partTo) {
+            return alert("Please Don't select Supplier To less than Supplier From");
+        }
+        if (partFrom == "") {
+            partFrom = "1215710010-00";
+        }
+        if (partTo == "") {
+            partTo = "C92290K010-00";
+        }
+
+        //console.log(partTo + " ", partFrom + " ");
+
+        // console.log(devDate + " devDate ", toDate + " toDate ", type + " type ", supFrom + " supfrom ", supTo + " supto ");
+
+        var reportUrl = "http://hmmt-app03/Reportserver/report/KB3/";
+
+        window.location.href = reportUrl + 'KBNCR160' + '?spcDateFrom=' + devDate + '&spcDateTo=' + toDate +
+            '&spcPartFrom=' + partFrom + '&spcPartTo=' + partTo;
+    });
+});
