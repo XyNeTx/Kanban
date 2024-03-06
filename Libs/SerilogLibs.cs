aed77f762cb367b1592dd93e4f3a53037d454c5c
20234111 <Sitthiporn_P@hinothailand.com>
2024-03-06 11:23:01 +0700

    KBNRT 280 : Complete

--- Libs/SerilogLibs.cs
+++ Libs/SerilogLibs.cs
@@ -18,5 +18,19 @@ namespace HINOSystem.Libs
                 throw;
             }
         }
+        public void WriteErrorLog(string Message, string UserName, string HostName)
+        {
+            try
+            {
+                Log.Error($"message : {Message} , username : {UserName} , hostname : {HostName}");
+            }
+            catch (Exception ex)
+            {
+                Log.Error($"message: {ex.Message}");
+                Console.WriteLine(ex.StackTrace);
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+        }
     }
 }
